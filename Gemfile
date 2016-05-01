e4056ece6f1045282f63824714cfc8be9c3dcfa6
Michael Busta <busta2356@gmail.com>
2016-05-01 16:39:01 -0400

    Update index View

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 ruby '2.3.0' # Or whichever Ruby version you are using.
 
 gem 'sinatra'
+gem 'httparty'
--- server.rb
+++ server.rb
@@ -3,3 +3,8 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/movie' do
+  movie = Siskel.new("Lion King")
+  "This movie title is #{movie.title}"
+end
